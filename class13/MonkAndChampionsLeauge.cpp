559089d81c5c9a505826ff307f06af0c18e9f5fd
Safayat Hossain <166735967+safayat00700@users.noreply.github.com>
2024-04-17 19:38:43 +0600

    added

--- class13/MonkAndChampionsLeauge.cpp
+++ class13/MonkAndChampionsLeauge.cpp
@@ -1,27 +1,15 @@
 //https://www.hackerearth.com/practice/data-structures/trees/heapspriority-queues/practice-problems/algorithm/monk-and-champions-league/description/
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-     int n,m;
-     cin>>n>>m;
-     priority_queue<int> q;
-     for(int i=0;i<n;i++){
-        int x;
-        cin>>x;
-        q.push(x);
-     }
-     long long ans =0;
-     for(int i =0;i<m;i++){
-        if(!q.empty()){
-            int a = q.top();//4 3 2 2
-            q.pop();
 
-            ans += a; //4+3+2+2
-            a--;//3 2 1 1
-            if(a>0){
-                q.push(a); //q =1 2 1 1
-            }
-        }
-     }
-     cout<<ans<<endl;
+
+int dsum(int x,int y){
+    return (x+y)*2;
+}
+
+
+int main(){
+     int a=2,b=3;
+     int result = dsum(a,b);
+     printf("%d",result);
 }
---
+++ class13/recursion.js
@@ -0,0 +1,9 @@
+function doubleSum(a ,b){
+    var result = (a+b)*2;
+    return result;
+}
+
+var a=5;
+var b =2;
+var result = doubleSum(a,b);
+console.log(result);
