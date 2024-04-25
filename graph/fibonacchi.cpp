2bfb6ef026254cfef4eabe4b35e06beb050eb3d6
Safayat Hossain <safayathossain40@gmail.com>
2024-04-25 22:32:14 +0600

    added

--- graph/fibonacchi.cpp
+++ graph/fibonacchi.cpp
@@ -1,22 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+const int mx = 1e7+123;
+int dp[mx];
 int fibo(int n){
+    //time complexity O(n)
     if(n==0){
         return 0;
     }
-    else if(n==1){
+    if(n==1){
         return 1;
     }
-    else{
-        return fibo(n-1)+fibo(n-2);
+    if(dp[n] != -1){
+        return dp[n];
     }
+    
+    return dp[n] = (fibo(n-1)+fibo(n-2));
+    
 }
 
 int main(){
-    int n;
-    cin>>n;
-    int x = fibo(n);
-    cout<<x<<end;
+    for(int i=0;i<1000;i++){
+        dp[i] = -1;
+    }
+    int x= fibo(6);
+    cout<<x<<" "<<endl;
+    
 
 }     
